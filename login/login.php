d625c6565f2250111773d1030bf3611e2fb1e646
Selelium <65858392+Selelium@users.noreply.github.com>
2022-01-19 16:09:06 +0900

    パスワードハッシュ処理実装

--- login/login.php
+++ login/login.php
@@ -12,23 +12,21 @@ if (empty($_POST["UserName"]) or empty($_POST["Password"])) { //ログイン名�
 //DB内で入力値に合致するアカウントを取得
 try {
 	$pdo = new PDO(DSN, DB_USER, DB_PASS);
-	$stmt = $pdo->prepare('select account_id from AccountTable where account_name = :name and account_pass = :password');	//入力値に合致するアカウントがあるか確認
+	$stmt = $pdo->prepare('select account_id, account_pass from AccountTable where account_name = :name');	//入力値に合致するアカウントがあるか確認
 	$stmt->bindValue(':name', $_POST["UserName"]);
-	$stmt->bindValue(':password', $_POST["Password"]);
 	$stmt->execute();
 	$row = $stmt->fetch();
-}catch (Exception $e) {
+} catch (Exception $e) {
 	echo $e->getMessage() . PHP_EOL;
 }
 
 //合致するアカウントがあるか
-if (!isset($row['account_id'])) {
-	$_SESSION['error_message'] = "ユーザーネームかパスワードが間違っています";
-	header("location: ./"); //ログインページへリダイレクト
-	exit();
-  }else{
+if (isset($row['account_id']) and password_verify($_POST["Password"], $row['account_pass'])) {	//IDとパスワードが正しい場合
 	$_SESSION['bs_account_id'] = $row['account_id'];	//セッションにIDを代入
 	header("location: ../collect"); //管理者ページへリダイレクト
 	exit();
-  }
-?>
+} else {
+	$_SESSION['error_message'] = "ユーザーネームかパスワードが間違っています";
+	header("location: ./"); //ログインページへリダイレクト
+	exit();
+}
